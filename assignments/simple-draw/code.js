3cb4844db8c48a24a1bf9410323d66a29caf08b0
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-08-18 15:05:45 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -46,3 +46,5 @@ drawFilledCircle(width - 50, height - 50, 50, 'red');
 drawFilledRect(5, height - 20, 45, 15, 'purple');
 
 drawCircle(width/3.5, height/10, 20, 'green');
+
+drawFilledCircle(width - 30, height - 50, 50, 'green');
