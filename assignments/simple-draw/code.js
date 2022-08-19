c78e0243157bd4746a29d600bef4ceb005767970
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-08-19 15:19:59 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -21,3 +21,21 @@ drawLine(0,0,width, height, 'green')
 drawLine(0,550,width, height, 'red')
 drawLine(0,200,width, height, 'green')
 drawLine(200,0,width, height, 'green')
+
+Intro to Programming
+JaxyPop 
+Welcome to your first programming exercise. This is very freeform. The comments in the initial code explain the functions and variables you have available.
+
+Put definitions here.
+14151617181920212223131112109876543
+drawLine(0,450,width, height, 'green')
+drawLine(0,400,width, height, 'red')
+drawLine(0,350,width, height, 'green')
+drawLine(0,300,width, height, 'red')
+drawLine(0,250,width, height, 'green')
+drawLine(0,200,width, height, 'green')
+drawLine(0,150,width, height, 'pink')
+drawLine(0,100,width, height, 'green')
+drawLine(0,50,width, height, 'pink')
+drawLine(500,0,width, height, 'green')
+
