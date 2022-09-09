365ad23e107e6d45ea684de98840561d25d9a325
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-09-09 15:20:50 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,23 +1,27 @@
-const add=(a,b) => {
-  return a+b
+const add = (a, b) => {
+  return a + b
 }
 
-const subtract=(a,b) => {
-  return a-b
+const subtract = (a, b) => {
+  return a - b
 }
 
-const multiply=(a,b) =>{
-  return a*b
+const multiply = (a, b) => {
+  return a * b
 }
 
-const divide=(a,b) => {
-  return a/b
+const divide = (a, b) => {
+  return a / b
 }
 
-const mod=(a,b) =>{
-  return a%b
+const mod = (a, b) => {
+  return a % b
 }
 
-const averageOfTwo=(a,b) =>{
-  return (a+b)/2
+const averageOfTwo = (a, b) => {
+  return (a + b) / 2
+}
+
+const averageOfThree = (a, b) => {
+  return (a + b) / 3
 }
