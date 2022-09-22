cb2613296e0faf3da6e80e48d0d97fcd900edbb2
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-09-22 14:57:06 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -67,4 +67,4 @@ drawFilledRect(5, height - 20, 45, 15, 'purple');
 drawTriangle(width * 0.25, 10, width * 0.75, 20, width * 0.5, 80, 'blue');
 
 // You can also put line breaks in if the line gets too long as shown
-drawFilledCircle(width / 3, height / 2, 10, 'blue');
+drawFilledCircle(width / 4, height / 2, 10, 'blue');
