561bc30fd81749949cf1d385b45fa401aaf2758f
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-09-22 15:14:20 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -62,4 +62,4 @@ drawFilledCircle(width / 6, height / 2, 20, 'blue');
 drawFilledCircle(width / 4, height / 2, 20, 'blue');
 drawFilledCircle(width / 12, height / 2, 20, 'blue');
 drawFilledCircle(width / 3000, height / 2, 20, 'blue');
-drawFilledCircle(width / 3, height / 2, 20, 'blue');
+drawFilledCircle(width / -243, height / 2, 20, 'blue');
