21626f90602b6ac61e750a55dad3fc4d7c7b807e
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-09-23 14:49:00 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -58,6 +58,8 @@
 
 // You can also put line breaks in if the line gets too long as shown
 
-const drawLineCircle(r) => {drawFilledCircle
-  for(let i=0; i< 100; x++)
+const drawLineCircle(r) => {
+  for(let i=0; i< width; i=i+2*r) {
+    drawFilledCircle(r+i, height/2, r, green)
+  }
 }
