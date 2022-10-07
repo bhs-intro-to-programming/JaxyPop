30bb139b631651d79627baf21c12c3e51382d2d2
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-10-07 14:58:22 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,15 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+const itemsLeftOver = (numberPeople, numberItmes) => numberPeople-numberItmes;
+
+const areaOfCircle = (r) => Math.PI(r)**2
+
+const volumeOfCube = (lengthEdge) => lengthEdge;
+
+const populationGrowth =(currentSize,growthRate) => (currentSize *(growthRate + 1));
+
+const earnedRunAverage = (earnedRuns, innings) => ((earnedRuns * innings)*9);
+
+const valueOfJewels = 
