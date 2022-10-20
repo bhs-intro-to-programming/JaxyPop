d7fca5be4e91b5e1d006c3a69c350cd310cf24ff
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-10-20 15:19:57 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,6 +1,6 @@
 const countTens = (numbers) => {
 let count = 0;
-for (let i = 0; i < numbers.length; i++)
+for (let i = 0; i < numbers.length; i++){
     if(numbers[i]===10){
       count++;
     }
