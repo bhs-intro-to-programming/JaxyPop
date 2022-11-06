0dfb0d6818cbce54b6db742a3dc0c803218d9eda
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-11-06 14:15:52 -0800

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -18,6 +18,52 @@ const maxRadius = (a,b) => {
  if a + b
 }
 
+const numCircles = (radius, width) => {
+  return Math.floor(width / (radius * 2));
+};
 
+const offset = (width, figureWidth) => {
+  return (width - figureWidth) / 2;
+};
+
+const canSleepIn = (weekday, vacation) => {
+  return !weekday || vacation;
+};
+
+const canGoToProm = (senior, invited, excluded) => {
+  return (senior || invited) && !excluded;
+};
+
+const getsSpeedingTicket = (mph, grouchy) => {
+  return mph > 70 || (grouchy && mph > 65);
+};
+
+const moreThanTwiceAsLong = (s1, s2) => {
+  return s1.length > s2.length * 2;
+};
+
+const aFartherThanB = (a, b, c) => {
+  return Math.abs(a - c) > Math.abs(b - c);
+};
+
+const firstHalf = (s) => {
+  return s.substring(0, s.length / 2);
+};
+
+const secondHalf = (s) => {
+  return s.substring(s.length / 2);
+};
+
+const upDown = (s) => {
+  return s.toUpperCase() + s.toLowerCase();
+};
+
+const everyOther = (s) => {
+  return s[0] + s[2] + s[4];
+};
+
+const upDownLastCharacter = (s) => {
+  return upDown(s[s.length - 1]);
+};
 
 
