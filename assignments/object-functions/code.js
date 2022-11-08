eaeff5624e007590706ac2caa8cac4baee37855e
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-11-08 15:33:37 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -10,8 +10,8 @@ const emptyObject = () => {
   return {}
 } 
 
-const distance = (x,y) => {
-  return Math.sqrt({})
+const distance = (p1,p2) => {
+  return Math.sqrt((p1.x-p2.x)**2)+((p1.y-p2.y)**2)
 }
 
 const midpoint =  (p1,p2) => {
