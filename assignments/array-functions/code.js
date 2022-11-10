87b35109aa93acb3442bf2c9f10ee11df30a6208
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-11-10 15:23:21 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,30 +1,38 @@
 const countTens = (numbers) => {
-let count = 0;
-for (let i = 0; i < numbers.length; i++){
-  if (numbers[i]===10) {
-      count ++;
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 10) {
+      count++;
     }
   }
-return count
+  return count
 }
 
 
 const sum = (numbers) => {
-let count = 0;
-for (let i = 0; i < numbers.length; i++){
-  count=count+numbers[i]
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    count = count + numbers[i]
 
-}
-return count
+  }
+  return count
 }
 
 const evens = (numbers) => {
-let result = [];
-for (let i = 0; i < numbers.length; i++){
- if(numbers[i]%2 ===0){
-   result.push(numbers[i])
- }
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result.push(numbers[i])
+    }
 
+  }
+  return result
 }
-return result
+
+const anyOverOneHundred = (numbers) => {
+  for (let i = 0; i<numbers.length; i++) {
+    if(numbers[i] > 100) {
+      return true
+          }
+  } return false
 }
