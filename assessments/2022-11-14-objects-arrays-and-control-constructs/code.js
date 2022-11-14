d0f8bc43075e52e86e3bc76acc2c86778b0b38ea
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-11-14 15:10:12 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,13 +2,19 @@
 // should save you some time.
 
 const area = (rect) => {
+  return rect.width * rect.height
 };
 
 const higherPaid = (e1, e2) => {
+  if(e1.salary - e2.salary) > (e2.salary - e1.salary)
+  return e1.salary + e2.salary
+  
 };
 
 const isSamePoint = (p1, p2) => {
-};
+  if(p1.x === p2.x) + (p1.y === p2.y)
+  return true
+ ;
 
 const totalWithTip = (bill, tipPercentage) => {
 };
