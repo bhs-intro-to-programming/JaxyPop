cfba090fba04b3d29a1d809f03e140186134f678
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-12-05 15:16:06 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -3,5 +3,20 @@ const upToX = (x) => {
 }
 
 const charactersAround = (x,s) => {
-  return s[s.indexOf(x)]
+  return s[s.indexOf(x)] , s[s.indexOf(x)]
 }
+
+const middle = (s) => {
+  return s.substring(s.length /4 , s.length -2 (s.length /4 ))
+}
+
+const pair = (s,x) => {
+  return s + 'and' + x;
+}
+
+const containsX = (s) => {
+  if (s.indexOf(x === -1)){
+    return false
+  }else 
+    return true 
+};
