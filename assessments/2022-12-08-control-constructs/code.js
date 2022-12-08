384274e019e1a5168821e959668a13b994da7d87
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-12-08 15:23:56 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -6,9 +6,13 @@ const countXs = (s) => {
   return s.substring
 }
 
-const timesTable = (nums) => {
-  if (emit())
-}
+const timesTable = (table) => {
+  for (let i = 0; i < table.length; i++) {
+    for (let j = 0; j < table.length; j++) {
+      table[i][j] = (i + 1) * (j + 1);
+    }
+  }
+};
 
 const containsX = () => {
   return s.substring('x') === true
