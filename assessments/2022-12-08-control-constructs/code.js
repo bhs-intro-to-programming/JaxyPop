0d971a304dfbe6af4858442ec825a2857bf4f65a
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-12-08 15:07:32 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

---
+++ assessments/2022-12-08-control-constructs/code.js
@@ -0,0 +1,19 @@
+const yesIfEven = (x) => {
+  return 
+}
+
+const countXs = (s) => {
+  return s.substring
+}
+
+const timesTable = (nums) => {
+  if (emit())
+}
+
+const containsX = () => {
+  return s.substring('x') === true
+}
+
+const sumSquares = () => {
+
+}
