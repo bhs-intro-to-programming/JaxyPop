fedbaa7f77a505eb88973dd7c7a71f5ac9402965
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-12-12 15:11:14 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -1,7 +1,12 @@
-const makeRow = ['','','']
-
-const makeBoard = [
+const makeRow = () => {
+  ['','','']
+}
+const makeBoard = () => {
+ [
   makeRow, makeRow, makeRow,
   makeRow,makeRow , makeRow,
   makeRow, makeRow, makeRow
 ]
+}
+const makeMove = (x , y , o) => {
+  let move = ('mark' = x , 'row' = y , 'column' = o )
