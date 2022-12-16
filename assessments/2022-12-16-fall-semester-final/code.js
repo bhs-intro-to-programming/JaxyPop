c3dadc67bde20a0d7a314904802eb485dcb8e6e4
JaxyPop <111395496+JaxyPop@users.noreply.github.com>
2022-12-16 12:06:26 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -157,10 +157,18 @@ const firstOk = (x) => {
 
 const timeToLeet = () => {
   if ( count = 0) 
-  let (!isLeet && random10k());{
+  let(!isLeet && random10k());{
   return count ++ }
 }
 
 const recordMove = () => {
-  return
+  
+}
+
+const willSeeTweet = (ft,fsrt,bt) => {
+  return (ft||fsrt||!bt)
+}
+
+const placeMark = (board, move ) => {
+  return board [move.row] , [move. coloumn] = move.mark;
 }
